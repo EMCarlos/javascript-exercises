c1014c9af2a99963807eabd0845cbca3cb2a1806
emcarlos <carlosemc12@gmail.com>
2023-04-22 19:53:09 -0300

    add fibonacci exercise solution

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,14 @@
-const fibonacci = function() {
+const fibonacci = function (num) {
+  let number = Number(num);
+  let fib = [0, 1];
 
+  for (let i = 2; i <= number; i++) {
+    fib[i] = fib[i - 1] + fib[i - 2];
+  }
+
+  if (number < 0) return "OOPS";
+  if (number <= 1) return number;
+  return fib[number];
 };
 
 // Do not edit below this line
