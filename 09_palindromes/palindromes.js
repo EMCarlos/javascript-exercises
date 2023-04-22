8998b7268f1893974baa64b04a7e7e65b8eeac5d
emcarlos <carlosemc12@gmail.com>
2023-04-22 19:52:57 -0300

    add palindrome exercise solution

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,9 @@
-const palindromes = function () {
+const palindromes = function (string) {
+  let re = /[\W_]/g;
+  let regString = string.toLowerCase().replace(re, "");
+  let reverseString = regString.split("").reverse().join("");
 
+  return regString === reverseString;
 };
 
 // Do not edit below this line
