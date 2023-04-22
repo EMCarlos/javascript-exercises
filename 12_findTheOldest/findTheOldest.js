7e7f0186fd059e7b58ac56994597cde18b293047
emcarlos <carlosemc12@gmail.com>
2023-04-22 20:48:59 -0300

    add solution to findTheOldest exercise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,33 @@
-const findTheOldest = function() {
+const findTheOldest = function (peoples) {
+  const oldestPeople = peoples.sort((a, b) => {
+    const lastPeople =
+      (a.yearOfDeath || new Date().getFullYear()) - a.yearOfBirth;
+    const nextPeople =
+      (b.yearOfDeath || new Date().getFullYear()) - b.yearOfBirth;
+    return lastPeople > nextPeople ? -1 : 1;
+  });
 
+  return oldestPeople[0];
 };
 
 // Do not edit below this line
 module.exports = findTheOldest;
+
+// ODIN SOLUTION
+// const findTheOldest = function (array) {
+//     return array.reduce((oldest, currentPerson) => {
+//       const oldestAge = getAge(oldest.yearOfBirth, oldest.yearOfDeath);
+//       const currentAge = getAge(
+//         currentPerson.yearOfBirth,
+//         currentPerson.yearOfDeath
+//       );
+//       return oldestAge < currentAge ? currentPerson : oldest;
+//     });
+//   };
+
+//   const getAge = function (birth, death) {
+//     if (!death) {
+//       death = new Date().getFullYear();
+//     }
+//     return death - birth;
+//   };
